02650019c3d4b6cb3b133c0e7c27b32ff1adc99c
Kevin <dragon0170@gmail.com>
2023-03-24 15:19:20 +0900

    Fix compile error for new modules

--- lab2/adder.v
+++ lab2/adder.v
@@ -1,4 +1,3 @@
 module Adder(input [31:0] in1, input [31:0] in2, output [31:0] dout);
-    always @(*) begin
-        sum = in1 + in2;
-    end
+    assign dout = in1 + in2;
+endmodule
--- lab2/mux.v
+++ lab2/mux.v
@@ -1,8 +1,3 @@
 module Mux(input [31:0] de_assert, input [31:0] assert, input sel, output [31:0] dout);
-    always @(*) begin
-        if (sel) begin
-            out = assert;
-        end else begin
-            out = de_assert;
-        end
-    end
+    assign dout = sel ? assert : de_assert;
+endmodule
