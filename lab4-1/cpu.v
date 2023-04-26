0e78af005db10855551b212d72a3fda0f598249c
Kevin <dragon0170@gmail.com>
2023-04-26 18:10:36 +0900

    Implement cpu module without data forwarding and hazard detection

--- lab4-1/cpu.v
+++ lab4-1/cpu.v
@@ -15,47 +15,72 @@ module CPU(input reset,       // positive reset signal
   wire [31:0] next_pc;
   wire [31:0] current_pc;
   wire [31:0] inst;
+  wire [31:0] rd_din;
+  wire [31:0] rs1_dout;
+  wire [31:0] rs2_dout;
+  wire [31:0] imm_gen_out;
+  wire [31:0] alu_in_2;
+  wire [31:0] alu_result;
+  wire [31:0] mem_dout;
+  wire alu_bcond; // TODO: will be used at control flow instruction feature
+  wire reg_write;
+  wire mem_read;
+  wire mem_write;
+  wire mem_to_reg;
+  wire pc_to_reg;
+  wire is_ecall;
+  wire [3:0] alu_op;
+  wire alu_src;
   /***** Register declarations *****/
   // You need to modify the width of registers
   // In addition, 
   // 1. You might need other pipeline registers that are not described below
   // 2. You might not need registers described below
   /***** IF/ID pipeline registers *****/
-  reg IF_ID_inst;           // will be used in ID stage
+  reg [31:0] IF_ID_inst;           // will be used in ID stage
   /***** ID/EX pipeline registers *****/
   // From the control unit
-  reg ID_EX_alu_op;         // will be used in EX stage
   reg ID_EX_alu_src;        // will be used in EX stage
   reg ID_EX_mem_write;      // will be used in MEM stage
   reg ID_EX_mem_read;       // will be used in MEM stage
   reg ID_EX_mem_to_reg;     // will be used in WB stage
   reg ID_EX_reg_write;      // will be used in WB stage
+  reg ID_EX_halt_cpu;      // will be used in WB stage
   // From others
-  reg ID_EX_rs1_data;
-  reg ID_EX_rs2_data;
-  reg ID_EX_imm;
-  reg ID_EX_ALU_ctrl_unit_input;
-  reg ID_EX_rd;
+  reg [31:0] ID_EX_rs1_data;
+  reg [31:0] ID_EX_rs2_data;
+  reg [31:0] ID_EX_imm;
+  reg [31:0] ID_EX_ALU_ctrl_unit_input;
+  reg [4:0] ID_EX_rd;
 
   /***** EX/MEM pipeline registers *****/
   // From the control unit
   reg EX_MEM_mem_write;     // will be used in MEM stage
   reg EX_MEM_mem_read;      // will be used in MEM stage
-  reg EX_MEM_is_branch;     // will be used in MEM stage
   reg EX_MEM_mem_to_reg;    // will be used in WB stage
   reg EX_MEM_reg_write;     // will be used in WB stage
+  reg EX_MEM_halt_cpu;      // will be used in WB stage
   // From others
-  reg EX_MEM_alu_out;
-  reg EX_MEM_dmem_data;
-  reg EX_MEM_rd;
+  reg [31:0] EX_MEM_alu_out;
+  reg [31:0] EX_MEM_dmem_data;
+  reg [4:0] EX_MEM_rd;
 
   /***** MEM/WB pipeline registers *****/
   // From the control unit
   reg MEM_WB_mem_to_reg;    // will be used in WB stage
   reg MEM_WB_reg_write;     // will be used in WB stage
+  reg MEM_WB_halt_cpu;      // will be used in WB stage
   // From others
-  reg MEM_WB_mem_to_reg_src_1;
-  reg MEM_WB_mem_to_reg_src_2;
+  reg [31:0] MEM_WB_mem_to_reg_src_1;
+  reg [31:0] MEM_WB_mem_to_reg_src_2;
+  reg [4:0] MEM_WB_rd;
+
+  // TODO: should be change when control flow instruction implemented
+  Adder pc_adder (
+    .in1(current_pc),
+    .in2(32'd4),
+    .dout(next_pc)
+  );
 
   // ---------- Update program counter ----------
   // PC must be updated on the rising edge (positive edge) of the clock.  
@@ -86,94 +111,158 @@ module CPU(input reset,       // positive reset signal
 
   // ---------- Register File ----------
   RegisterFile reg_file (
-    .reset (),        // input
-    .clk (),          // input
-    .rs1 (),          // input
-    .rs2 (),          // input
-    .rd (),           // input
-    .rd_din (),       // input
-    .write_enable (),    // input
-    .rs1_dout (),     // output
-    .rs2_dout ()      // output
+    .reset (reset),        // input
+    .clk (clk),          // input
+    .rs1 (is_ecall ? 5'b10001 : IF_ID_inst[19:15]),  // input
+    .rs2 (IF_ID_inst[24:20]),          // input
+    .rd (MEM_WB_rd),           // input
+    .rd_din (rd_din),       // input
+    .write_enable (MEM_WB_reg_write),    // input
+    .rs1_dout (rs1_dout),     // output
+    .rs2_dout (rs2_dout)      // output
   );
 
 
   // ---------- Control Unit ----------
   ControlUnit ctrl_unit (
-    .part_of_inst(),  // input
-    .mem_read(),      // output
-    .mem_to_reg(),    // output
-    .mem_write(),     // output
-    .alu_src(),       // output
-    .write_enable(),  // output
-    .pc_to_reg(),     // output
-    .alu_op(),        // output
-    .is_ecall()       // output (ecall inst)
+    .part_of_inst(IF_ID_inst[6:0]),  // input
+    .mem_read(mem_read),      // output
+    .mem_to_reg(mem_to_reg),    // output
+    .mem_write(mem_write),     // output
+    .alu_src(alu_src),       // output
+    .write_enable(reg_write),  // output
+    .pc_to_reg(pc_to_reg),     // output
+    .is_ecall(is_ecall)       // output (ecall inst)
   );
 
   // ---------- Immediate Generator ----------
   ImmediateGenerator imm_gen(
-    .part_of_inst(),  // input
-    .imm_gen_out()    // output
+    .part_of_inst(IF_ID_inst),  // input
+    .imm_gen_out(imm_gen_out)    // output
   );
 
   // Update ID/EX pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
+      ID_EX_alu_src <= 0;
+      ID_EX_mem_write <= 0;
+      ID_EX_mem_read <= 0;
+      ID_EX_mem_to_reg <= 0;
+      ID_EX_reg_write <= 0;
+      ID_EX_halt_cpu <= 0;
+
+      ID_EX_rs1_data <= 0;
+      ID_EX_rs2_data <= 0;
+      ID_EX_imm <= 0;
+      ID_EX_ALU_ctrl_unit_input <= 0;
+      ID_EX_rd <= 0;
     end
     else begin
+      ID_EX_alu_src <= alu_src;
+      ID_EX_mem_write <= mem_write;
+      ID_EX_mem_read <= mem_read;
+      ID_EX_mem_to_reg <= mem_to_reg;
+      ID_EX_reg_write <= reg_write;
+      ID_EX_halt_cpu <= is_ecall && rs1_dout == 10;
+
+      ID_EX_rs1_data <= rs1_dout;
+      ID_EX_rs2_data <= rs2_dout;
+      ID_EX_imm <= imm_gen_out;
+      ID_EX_ALU_ctrl_unit_input <= IF_ID_inst;
+      ID_EX_rd <= IF_ID_inst[11:7];
     end
   end
 
+  // Mux for alu_in_2
+  Mux2To1 alu_in_2_mux(
+    .din0(ID_EX_rs2_data),
+    .din1(ID_EX_imm),
+    .sel(ID_EX_alu_src),
+    .dout(alu_in_2)
+  );
+
   // ---------- ALU Control Unit ----------
   ALUControlUnit alu_ctrl_unit (
-    .part_of_inst(),  // input
-    .alu_op()         // output
+    .part_of_inst(ID_EX_ALU_ctrl_unit_input),  // input
+    .alu_op(alu_op)         // output
   );
 
   // ---------- ALU ----------
   ALU alu (
-    .alu_op(),      // input
-    .alu_in_1(),    // input  
-    .alu_in_2(),    // input
-    .alu_result(),  // output
-    .alu_zero()     // output
+    .alu_op(alu_op),      // input
+    .alu_in_1(ID_EX_rs1_data),    // input  
+    .alu_in_2(alu_in_2),    // input
+    .alu_result(alu_result),  // output
+    .alu_bcond(alu_bcond)     // output
   );
 
   // Update EX/MEM pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
+      EX_MEM_mem_write <= 0;
+      EX_MEM_mem_read <= 0;
+      EX_MEM_mem_to_reg <= 0;
+      EX_MEM_reg_write <= 0;
+      EX_MEM_halt_cpu <= 0;
+
+      EX_MEM_alu_out <= 0;
+      EX_MEM_dmem_data <= 0;
+      EX_MEM_rd <= 0;
     end
     else begin
+      EX_MEM_mem_write <= ID_EX_mem_write;
+      EX_MEM_mem_read <= ID_EX_mem_read;
+      EX_MEM_mem_to_reg <= ID_EX_mem_to_reg;
+      EX_MEM_reg_write <= ID_EX_reg_write;
+      EX_MEM_halt_cpu <= ID_EX_halt_cpu;
+
+      EX_MEM_alu_out <= alu_result;
+      EX_MEM_dmem_data <= ID_EX_rs2_data;
+      EX_MEM_rd <= ID_EX_rd;
     end
   end
 
   // ---------- Data Memory ----------
   DataMemory dmem(
-    .reset (),      // input
-    .clk (),        // input
-    .addr (),       // input
-    .din (),        // input
-    .mem_read (),   // input
-    .mem_write (),  // input
-    .dout ()        // output
+    .reset (reset),      // input
+    .clk (clk),        // input
+    .addr (EX_MEM_alu_out),       // input
+    .din (EX_MEM_dmem_data),        // input
+    .mem_read (EX_MEM_mem_read),   // input
+    .mem_write (EX_MEM_mem_write),  // input
+    .dout (mem_dout)        // output
   );
 
   // Update MEM/WB pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
+      MEM_WB_mem_to_reg <= 0;
+      MEM_WB_reg_write <= 0;
+      MEM_WB_halt_cpu <= 0;
+
+      MEM_WB_mem_to_reg_src_1 <= 0;
+      MEM_WB_mem_to_reg_src_2 <= 0;
+      MEM_WB_rd <= 0;
     end
     else begin
+      MEM_WB_mem_to_reg <= EX_MEM_mem_to_reg;
+      MEM_WB_reg_write <= EX_MEM_reg_write;
+      MEM_WB_halt_cpu <= EX_MEM_halt_cpu;
+
+      MEM_WB_mem_to_reg_src_1 <= EX_MEM_alu_out;
+      MEM_WB_mem_to_reg_src_2 <= mem_dout;
+      MEM_WB_rd <= EX_MEM_rd;
     end
   end
 
+  // Mux for mem_to_reg
+  Mux2To1 mem_to_reg_mux(
+    .din0(MEM_WB_mem_to_reg_src_1),
+    .din1(MEM_WB_mem_to_reg_src_2),
+    .sel(MEM_WB_mem_to_reg),
+    .dout(rd_din)
+  );
 
-
-  Adder pc_adder (
-    .in1(current_pc),
-    .in2(1d'4)
-    .dout(next_pc)
-    );
-
+  assign is_halted = MEM_WB_halt_cpu;
   
 endmodule
